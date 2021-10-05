86ba1fbe4391b89a9d011152c9706114b2b40707
ukaran11 <ukaran11@gmail.com>
2021-10-06 02:39:14 +0530

    Utsav | Added basic user validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,20 @@
 const User = require("../models/User");
 
+// Handle Errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: ''};
+
+    // validation errors
+    if(err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            console.log(properties)
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -15,8 +30,8 @@ module.exports.signup_post = async (req, res) => {
         const user = await User.create({ email, password});
         res.status(201).json(user);
     } catch (err) {
-        console.log(err);
-        res.status(400).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 }
 
