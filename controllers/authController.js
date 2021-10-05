78582ff0bab1d5ebfedf381abaa36ff6f429e354
ukaran11 <ukaran11@gmail.com>
2021-10-06 03:02:41 +0530

    Utsav | Handled duplicate user signup validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,12 @@ const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: ''};
 
+    // duplicate error code
+    if(err.code == 11000) {
+        errors.email = "That email is already registered"
+        return errors;
+    }
+
     // validation errors
     if(err.message.includes('user validation failed')) {
         Object.values(err.errors).forEach(({ properties }) => {
