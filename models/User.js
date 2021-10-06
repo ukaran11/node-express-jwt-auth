1855731d1c3e428f8ff89bc23bac9d79421e406a
ukaran11 <ukaran11@gmail.com>
2021-10-06 23:11:39 +0530

    Utsav | Added password hashing through bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -16,17 +17,21 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// fire a function after doc saved to db
-userSchema.post('save', (doc, next) => {
-    console.log('new user was created and saved', doc)
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+    // console.log('user about to be created & saved', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
     next();
 })
 
-// fire a function before doc saved to db
-userSchema.pre('save', function(next) {
-    console.log('user about to be created & saved', this);
-    next();
-})
+// fire a function after doc saved to db
+// userSchema.post('save', (doc, next) => {
+//     console.log('new user was created and saved', doc)
+//     next();
+// })
 
 const User = mongoose.model('user', userSchema);
 
