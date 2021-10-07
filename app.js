fb62fbc8c476d7834b6dc7294f27fc48032ee837
ukaran11 <ukaran11@gmail.com>
2021-10-08 00:25:56 +0530

    Utsav | Added cookie parsing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
 
@@ -13,6 +14,7 @@ console.log(dbURI)
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -29,3 +31,18 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true')
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, {maxAge: 1000* 60 * 60 * 24, httpOnly: true})
+  res.send('you got the cookies');
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies)
+
+  res.json(cookies)
+})
