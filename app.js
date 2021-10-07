0cd7b62c3bf91728c4ddb14cdbf60e019e2dbdaf
ukaran11 <ukaran11@gmail.com>
2021-10-08 01:04:29 +0530

    Utsav | Added jwt while signup

--- app.js
+++ app.js
@@ -32,17 +32,16 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-app.get('/set-cookies', (req, res) => {
+// app.get('/set-cookies', (req, res) => {
+//   // res.setHeader('Set-Cookie', 'newUser=true')
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, {maxAge: 1000* 60 * 60 * 24, httpOnly: true})
+//   res.send('you got the cookies');
+// })
 
-  // res.setHeader('Set-Cookie', 'newUser=true')
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, {maxAge: 1000* 60 * 60 * 24, httpOnly: true})
-  res.send('you got the cookies');
-})
+// app.get('/read-cookies', (req, res) => {
+//   const cookies = req.cookies;
+//   console.log(cookies)
 
-app.get('/read-cookies', (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies)
-
-  res.json(cookies)
-})
+//   res.json(cookies)
+// })
