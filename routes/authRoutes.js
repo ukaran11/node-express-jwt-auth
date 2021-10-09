1cc27a2810bd4fea21ae64391b7eaad5df107bbe
Utsav Karan <ukaran11@gmail.com>
2021-10-09 16:45:18 +0530

    Utsav | Added logout functionality

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,6 @@ router.get('/signup', authControlller.signup_get);
 router.post('/signup', authControlller.signup_post);
 router.get('/login', authControlller.login_get);
 router.post('/login', authControlller.login_post);
+router.get('/logout', authControlller.logout_get);
 
 module.exports = router;
